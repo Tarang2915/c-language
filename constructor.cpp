8150d3da728ddab5e137d8494be7f4aeee3e5bd2
Tarang2915 <139239321+Tarang2915@users.noreply.github.com>
2023-07-19 10:51:15 +0530

    New 19-07-2023

---
+++ constructor.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Result
+{
+    int adminno;
+    char sname[20];
+    float eng, maths, sci, total;
+
+    float ctotal()
+    {
+        return eng + maths + sci;
+    }
+
+public:
+    Result()
+    {
+        cout << "Enter Admin Number: ";
+        cin >> adminno;
+        cout << "Enter Name: ";
+        cin >> sname;
+        cout << "Enter Maths Marks: ";
+        cin >> maths;
+        cout << "Enter Science Marks: ";
+        cin >> sci;
+        cout << "Enter English Marks: ";
+        cin >> eng;
+
+        total = ctotal();
+    }
+
+    void showdata()
+    {
+        cout << "Admin Number: " << adminno << endl;
+        cout << "Student Name: " << sname << endl;
+        cout << "Science Marks: " << sci << endl;
+        cout << "Maths Marks: " << maths << endl;
+        cout << "English Marks: " << eng << endl;
+        cout << "Total Marks: " << total << endl;
+    }
+};
+
+int main()
+{
+    Result s;
+
+    s.showdata();
+}
