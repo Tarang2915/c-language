b2090f44582dc76a710eaea6de3621489c7a4463
Tarang2915 <139239321+Tarang2915@users.noreply.github.com>
2023-07-24 10:55:51 +0530

    24-07-2023

---
+++ unaovr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Box
+{
+    int l, b, h;
+
+public:
+    void getdata(int x, int y, int z)
+    {
+        l = x, b = y, h = z;
+    }
+
+    float displaydata()
+    {
+        return l * b * h;
+    }
+
+    Box operator--(int)
+    {
+        Box t;
+
+        t.l = l--;
+        t.b = b--;
+        t.h = h--;
+
+        return t;
+    }
+};
+
+int main()
+{
+    Box a, b;
+
+    a.getdata(2, 3, 4);
+    cout << "Volume of Box A: " << a.displaydata() << endl;
+
+    a--;
+    b = a;
+    cout << "Volume of Box B: " << b.displaydata() << endl;
+}
