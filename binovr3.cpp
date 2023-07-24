7e1d9b1528325dfa94e2d637bd80e8922d0a0510
Tarang2915 <139239321+Tarang2915@users.noreply.github.com>
2023-07-24 11:47:07 +0530

    24-07-2023

---
+++ binovr3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Box
+{
+    int l, b, h;
+
+public:
+    void getdata(int x, int y, int z)
+    {
+        l = x, b = y, h = z;
+    }
+
+    float displaydata()
+    {
+        return l * b * h;
+    }
+
+    Box operator/(Box &n)
+    {
+        Box t;
+
+        t.l = l / n.l;
+        t.b = b / n.b;
+        t.h = h / n.h;
+
+        return t;
+    }
+};
+
+int main()
+{
+    Box a, b, c;
+
+    a.getdata(6, 12, 6);
+    cout << "Volume of Box A: " << a.displaydata() << endl;
+
+    b.getdata(3, 4, 5);
+    cout << "Volume of Box B: " << b.displaydata() << endl;
+
+    c = a / b;
+    cout << "Volume of Box c: " << c.displaydata() << endl;
+}
