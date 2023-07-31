3f8d9dc72b3779fa22d85109b6598ba9c330961a
Tarang2915 <139239321+Tarang2915@users.noreply.github.com>
2023-07-31 12:05:53 +0530

    31-07-2023

---
+++ math.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    cout << "Ans: " << hypot(12, 5) << endl;
+    cout << "Ans: " << asin(-1) << endl;
+    cout << "Ans: " << log(2.718281828459045) << endl;
+}
